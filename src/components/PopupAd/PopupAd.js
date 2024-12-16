51c625db98d26138d22fbc5c915b6dd7b670c407
ififiam <ifififam1@gmail.com>
2024-12-16 15:31:48 +0900

    팝업창 기능 구현 및 스타일 최적화

---
+++ src/components/PopupAd/PopupAd.js
@@ -0,0 +1,7 @@
+document.getElementById("close-today").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+});
+
+document.getElementById("close-popup").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+}); 
