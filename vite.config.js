0c58edba576148d13bec12313da596c045a42354
nurungzzy <duswl0801@khu.ac.kr>
2024-12-19 17:03:02 +0900

    [Feat] vite.config.js 수정
    - 상품 목록 페이지 경로 추가

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
-// import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
-  build: {
-    outDir:'docs',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
   },
-})
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+    assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg'],
+    rollupOptions: {
+      input: {
+        productList: resolve(__dirname, 'src/pages/productList/index.html'),
+      },
+    },
+  },
+});
