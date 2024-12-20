ebe88fbecfe6f34fd13233616df4a185063b53f9
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-12-20 18:07:38 +0900

    [dfs]dsfd
    -
    -
    
    #

--- src/components/main/todayRecommendProducts.js
+++ src/components/main/todayRecommendProducts.js
@@ -1,55 +1,59 @@
 import { LitElement, html, css } from 'lit';
 import resetCSS from '@/Layout/resetCSS';
 import '../ProductCard/ProductCard.js';
+import pb from '@/api/pocketbase';
 
 export class TodayRecommendProducts extends LitElement {
   constructor() {
     super();
     this.sectionTitle = '이 상품 어때요?';
-    this.products = [
-      {
-        image: '/assets/images/product01.webp',
-        title: '[풀무원] 탱탱쫄면 (4개입)',
-        price: 4980,
-        originalPrice: 4980,
-        isDiscounted: false,
-        discount: 0,
-        badges: [],
-      },
-      {
-        image: '/assets/images/product02.webp',
-        delivery: '샛별배송',
-        title: '[온더바디] 조르디 시카 자석 선쿠션',
-        price: 32500,
-        originalPrice: 42000,
-        isDiscounted: true,
-        discount: 24,
-        badges: [
-          { type: 'kurly', text: 'Kurly Only' },
-          { type: 'limit', text: '한정수량' },
-        ],
-      },
-      {
-        image: '/assets/images/product03.webp',
-        delivery: '샛별배송',
-        title: '유기농 밀키퀸 현미 4kg',
-        price: 25000,
-        originalPrice: 25000,
-        isDiscounted: false,
-        discount: 0,
-        badges: [],
-      },
-      {
-        image: '/assets/images/product04.webp',
-        delivery: '샛별배송',
-        title: '[프로쉬] 베이비 세탁세',
-        price: 18900,
-        originalPrice: 24000,
-        isDiscounted: true,
-        discount: 24,
-        badges: [],
-      },
-    ];
+    this.products = [];
+
+    this.fetchData()
+    // this.products = [
+    //   {
+    //     image: '/assets/images/product01.webp',
+    //     title: '[풀무원] 탱탱쫄면 (4개입)',
+    //     price: 4980,
+    //     originalPrice: 4980,
+    //     isDiscounted: false,
+    //     discount: 0,
+    //     badges: [],
+    //   },
+    //   {
+    //     image: '/assets/images/product02.webp',
+    //     delivery: '샛별배송',
+    //     title: '[온더바디] 조르디 시카 자석 선쿠션',
+    //     price: 32500,
+    //     originalPrice: 42000,
+    //     isDiscounted: true,
+    //     discount: 24,
+    //     badges: [
+    //       { type: 'kurly', text: 'Kurly Only' },
+    //       { type: 'limit', text: '한정수량' },
+    //     ],
+    //   },
+    //   {
+    //     image: '/assets/images/product03.webp',
+    //     delivery: '샛별배송',
+    //     title: '유기농 밀키퀸 현미 4kg',
+    //     price: 25000,
+    //     originalPrice: 25000,
+    //     isDiscounted: false,
+    //     discount: 0,
+    //     badges: [],
+    //   },
+    //   {
+    //     image: '/assets/images/product04.webp',
+    //     delivery: '샛별배송',
+    //     title: '[프로쉬] 베이비 세탁세',
+    //     price: 18900,
+    //     originalPrice: 24000,
+    //     isDiscounted: true,
+    //     discount: 24,
+    //     badges: [],
+    //   },
+    // ];
   }
 
   static get properties() {
@@ -87,6 +91,42 @@ export class TodayRecommendProducts extends LitElement {
       `,
     ];
   }
+
+  //  connectedCallback() {
+  //   super.connectedCallback();
+
+  // }
+
+
+  async fetchData(){
+    console.log('ggg');
+
+    try {
+      // 페이지네이션을 위해 getList 사용시
+      // const data = await pb.collection('Products').getList(1, 10);
+
+      
+
+      const resultList = await pb.collection('Products').getList(1, 50);
+      console.log(resultList)
+      // 모든 데이터를 한번에 가져오려면
+      // const data = await pb.collection('products').getFullList();
+      
+      this.products = data.items.map((item) => ({
+        image: item.image || '/assets/images/product01.webp',
+        // delivery: item.delivery |s| '일반배송',
+        title: item.title || '제품명 없음',
+        price: item.price || 0,
+        // originalPrice: item.originalPrice || 0,
+        // isDiscounted: item.isDiscounted || false,
+        // discount: item.discount || 0,
+        // badges: item.badges || [],
+      }));
+    } catch (error) {
+      console.error('Pocketbase 데이터 가져오기 실패:', error);
+    }
+  }
+
 
   render() {
     return html`
