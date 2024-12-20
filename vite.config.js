85b682701ff3ef90a06d8cfdde22bd08cafe6ddb
sungwoo00 <dhdpdhdp5899@gmail.com>
2024-12-20 10:34:48 +0900

    [Chore] 배포 사이트 라우팅 처리
    -  Vercel 배포 환경 라우팅 처리 추가
    -  로컬, 배포 환경에 따른 동적 라우팅 처리 추가
    - gitignore .json 제거
    
    #77

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,35 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 
+const getBaseUrl = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return 'https://7th-market-carly.vercel.app';
+  }
+  return 'http://localhost:5173';
+};
 export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
   },
+  base: './',
   server: {
     proxy: {
       '/login': {
-        target: 'http://localhost:5173',
+        target: getBaseUrl(),
         rewrite: () => '/src/pages/login/index.html'
       },
       '/register': {
-        target: 'http://localhost:5173',
+        target: getBaseUrl(),
         rewrite: () => '/src/pages/register/index.html'
       },
       '/productDetail': {
-        target: 'http://localhost:5173',
+        target: getBaseUrl(),
         rewrite: () => '/src/pages/productDetail/index.html'
       },
       '/popup': {
-        target: 'http://localhost:5173',
+        target: getBaseUrl(),
         rewrite: () => '/src/components/PopupAd/index.html'
       }
     }
@@ -32,14 +39,14 @@ export default defineConfig({
     emptyOutDir: true,
     assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg'],
     // rollupOptions 삭제 가능 (위의 proxy가 라우팅을 해줌)
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'index.html'),
-        'product': resolve(__dirname, 'src/pages/productDetail/index.html'),
-        'login': resolve(__dirname, 'src/pages/login/index.html'),
-        'register': resolve(__dirname, 'src/pages/register/index.html'),
-        'popup': resolve(__dirname, 'src/components/PopupAd/index.html'),
-      },
-    },
+    // rollupOptions: {
+    //   input: {
+    //     index: resolve(__dirname, 'index.html'),
+    //     'product': resolve(__dirname, 'src/pages/productDetail/index.html'),
+    //     'login': resolve(__dirname, 'src/pages/login/index.html'),
+    //     'register': resolve(__dirname, 'src/pages/register/index.html'),
+    //     'popup': resolve(__dirname, 'src/components/PopupAd/index.html'),
+    //   },
+    // },
   },
 });
